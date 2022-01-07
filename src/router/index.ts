408d6ab458802aef161624e89c05106b76fc3cab
Felix Orinda <forinda82@gmail.com>
2022-01-07 15:52:21 +0300

    API docs setup Fixup#2 YML->JSON

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,8 @@ import { Application } from 'express'
 import AuthRouter from '../api/routes/Auth.routes'
 import WorkspaceRouter from '../api/routes/Workspace.routes'
 import swaggerUi from 'swagger-ui-express'
-import YAML from 'yamljs'
-import path from 'path'
-import { BASE_DIR } from './../config'
+import DOCS from './../docs/api.json'
 
-const DOCS = YAML.load(path.join(BASE_DIR, '/docs/api.yml'))
 
 export default ({ app }: { app: Application }) => {
 	app.use('/auth', AuthRouter)
