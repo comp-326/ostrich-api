b562ece6bfb62532c9ddb1e137e789b827fdab56
Felix Orinda <forinda82@gmail.com>
2022-01-07 16:46:46 +0300

    Homepage#1 update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,20 @@
+import { homepage } from './../html'
 import { Application } from 'express'
 import AuthRouter from '../api/routes/Auth.routes'
 import WorkspaceRouter from '../api/routes/Workspace.routes'
 import swaggerUi from 'swagger-ui-express'
 import DOCS from './../docs/api.json'
 
-
 export default ({ app }: { app: Application }) => {
 	app.use('/auth', AuthRouter)
 	app.use('/workspace', WorkspaceRouter)
 	app.use(
-		'/',
+		'/docs',
 		swaggerUi.serve,
 		swaggerUi.setup(DOCS, { customSiteTitle: 'Ostrich App' }),
 	)
+	app.get('/', (req, res) => {
+		res.send(homepage)
+	})
 	return app
 }
