7010f18b80400201326671584568a8bd3e098820
Felix Orinda <forinda82@gmail.com>
2022-01-07 12:37:46 +0300

    Authorization and authentication

--- src/api/controllers/Auth.controller.ts
+++ src/api/controllers/Auth.controller.ts
@@ -1,23 +1,23 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Request, Response, NextFunction } from 'express'
+import { Response, NextFunction } from 'express'
 import ErrorResponse from './../../middlewares/error'
 import User from './../../model/User.model'
 import jwt from 'jsonwebtoken'
 import { SECRET_KEY } from './../../config'
+import { RequestType } from './types'
+import { mailTransport, resetPasswordTemplate } from '../services/Mail.service'
 
 export const login = async (
-	req: Request,
+	req: RequestType,
 	res: Response,
 	next: NextFunction,
 ) => {
 	try {
 		const { email } = req.body
 		const user = await User.findOne({ email }).select('+password')
-
-		const p_match = await user!.passwordMatch(req.body.password)
-
-		if (!p_match)
+		if (!(await user!.passwordMatch(req.body.password)))
 			return res
 				.status(401)
 				.json({ success: false, message: 'Check your login details' })
@@ -48,7 +48,7 @@ export const login = async (
 }
 
 export const register = async (
-	req: Request,
+	req: RequestType,
 	res: Response,
 	next: NextFunction,
 ) => {
@@ -61,11 +61,35 @@ export const register = async (
 		if (!newUser)
 			throw new ErrorResponse('Account could not be created', 500)
 		const { password, ...props } = newUser._doc
+		const confirmToken = jwt.sign(
+			{ userId: newUser._id },
+			process.env.SECRET_KEY!,
+		)
+		console.log(newUser)
+
+		mailTransport.sendMail(
+			{
+				to: newUser.email,
+				subject: 'Activate your account',
+				html: `
+			<div>
+			<p>Please click the link below to activate your account</p>
+			<a href="http://localhost:3000/auth/account/confirm/${confirmToken}>Activate account</a>
+			</div>
+			`,
+			},
+			async (err, info) => {
+				if (err) {
+					console.log('Could not send email', err)
+				}
+				if (info) console.log('Email sent successfully')
+			},
+		)
 		return res.status(200).json({
 			success: true,
 			user: props,
 			message:
-				'User account created please check your email to confirm your account',
+				'Account created successfully, Check your email for activaioni',
 		})
 	} catch (error) {
 		return next(error)
@@ -73,7 +97,7 @@ export const register = async (
 }
 
 export const confirmAccountEmail = async (
-	req: Request,
+	req: RequestType,
 	res: Response,
 	next: NextFunction,
 ) => {
@@ -84,12 +108,25 @@ export const confirmAccountEmail = async (
 		}
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		return jwt.verify(token, SECRET_KEY!, async (err, payload) => {
-			if (err) throw new ErrorResponse('Invalid or expired token', 400)
+			if (err)
+				throw new ErrorResponse('Confirmation ink has expired', 400)
+			if (
+				await User.findOne({
+					$and: [{ _id: payload!.userId }, { active: true }],
+				})
+			) {
+				return next(
+					new ErrorResponse(
+						'Account already activated proceed to login',
+						400,
+					),
+				)
+			}
 			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-			const user = await User.findById(payload!.userId)
-			if (user?.active)
-				throw new ErrorResponse('Account already registered', 400)
-			await User.updateOne({ active: true })
+			await User.findByIdAndUpdate(payload!.userId, {
+				active: true,
+			})
+
 			return res.status(200).json({
 				message: 'Account activated successfully',
 				success: true,
@@ -101,7 +138,7 @@ export const confirmAccountEmail = async (
 }
 
 export const deleteUserAccount = async (
-	req: Request,
+	req: RequestType,
 	res: Response,
 	next: NextFunction,
 ) => {
@@ -110,5 +147,64 @@ export const deleteUserAccount = async (
 	return res.status(200).json({ message: 'Register' })
 	// } catch (e) {
 	// next(e)
+	// }
+}
+
+export const forgotPassword = async (
+	req: RequestType,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const { email }: { email: string } = req.body
+		const user = await User.findOne({ email })
+		if (!user)
+			return next(new ErrorResponse('Account email does not exist', 404))
+		const token = jwt.sign(
+			{ userId: user._id, email: user.email },
+			SECRET_KEY!,
+			{ expiresIn: '1h' },
+		)
+		mailTransport.sendMail(
+			{
+				to: user.email,
+				subject: 'Password reset',
+				html: resetPasswordTemplate(token),
+			},
+			async (err, _payload) => {
+				if (err) {
+					return next(
+						new ErrorResponse(
+							'Please check your email to reset your password',
+							400,
+						),
+					)
+					return
+				}
+			},
+		)
+
+		return res.status(200).json({ message: 'Register' })
+	} catch (e) {
+		next(e)
+	}
+}
+export const resetPassword = async (
+	req: RequestType,
+	res: Response,
+	next: NextFunction,
+) => {
+	// try {
+	// 	// const user = await User.delete({ ...req.body })
+	// 	const password = req.body.password
+	// 	const decoded = jwt.decode(req.params.token)
+	// 	await User.findByIdAndUpdate(
+
+	// 		{ password },
+	// 		{ new: true },
+	// 	)
+	return res.status(200).json({ message: 'Resetting password' })
+	// } catch (e) {
+	// 	next(e)
 	// }
 }
