b0b44329e4a382f522ddb3cc4f791e0bc9e71e67
Felix Orinda <forinda82@gmail.com>
2022-04-10 03:43:59 +0300

    Update linting

--- src/uploadSDK/index.ts
+++ src/uploadSDK/index.ts
@@ -1,10 +1,10 @@
+import { v2 as cloudinary } from "cloudinary"
 import {
 	CLOUDINARY_NAME,
 	CLOUDINARY_API_KEY,
 	CLOUDINARY_SECRET,
 } from "@root/config"
 
-import { v2 as cloudinary } from "cloudinary"
 
 export default cloudinary.config({
 	cloud_name: CLOUDINARY_NAME,
