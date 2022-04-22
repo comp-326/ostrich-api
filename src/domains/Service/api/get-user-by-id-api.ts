380b25cc67a0cf357aa9a3014efa9466a5dbdfee
Felix Ochieng Orinda <forinda82@gmail.com>
2022-04-22 07:50:38 +0300

    Fix Express error argument and delete service domain

--- src/domains/Service/api/get-user-by-id-api.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import MakeRequestAdapter from "../adapters"
-import { findById } from "../controllers"
-
-const qRouter = Router()
-export async function makeQueryIdUserApiCall(app: Router) {
-	app.use("/single", qRouter)
-	qRouter.get("/:id", MakeRequestAdapter(findById))
-}
