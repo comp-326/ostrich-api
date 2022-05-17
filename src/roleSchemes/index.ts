f0e72e380a0a86d4151a40f23ddcfe8c5a203eb0
Felix Orinda <forinda82@gmail.com>
2022-05-17 16:57:42 +0300

    Update role migration script

--- src/roleSchemes/index.ts
+++ src/roleSchemes/index.ts
@@ -1,86 +1,34 @@
 import { environmentConfig } from '@ostrich-app-config';
-import userRoles from '@ostrich-app/features/userRoles/models';
-import workspaceRoles from '@ostrich-app/features/workspaceRoles/models';
+import userRoleModel from '@ostrich-app/features/userRoles/models';
+import workspaceRoleModel from '@ostrich-app/features/workspaceRoles/models';
 
 class RoleMigrator {
 
 
-	private migrateUserPermissions = () => {
-		userRoles.getDefaultRole().then(async (res) => {
-			if (res) {
-				console.log('Default user role exists migrating updates-----------');
-				userRoles.InsertRoles().then(() => {
-					console.log('Roles migrated successfully-------------------');
+	private migrateUserPermissions = async () => {
 
-				}).catch(err => {
-					console.log(err);
-
-				});
-			}
-			if (!res) {
-				console.log('User roles do not exist creating------------------');
-				console.log('User roles not migrated doing migrations----');
-				userRoles.InsertRoles().then(() => {
-					console.log('Roles migrated successfully----------------------');
-
-				}).catch(err => {
-					console.log(err);
-
-				});
-				console.log('User roles migration success------');
-
-			}
-		}
-		).catch(err => {
-			console.log(err);
-		});
+		await userRoleModel.InsertRoles();
 
 	};
 
-	private migrateWorkspacePermissions = () => {
-		workspaceRoles.getDefaultRole().then(async (res) => {
-			if (res)
-				console.log('Workspace default role exists migrating updates----------');
-
-			if (!res) {
-				console.log('Workspace roles do not exist creating-----------');
-				console.log('User roles not migrated doing migrations----');
-				workspaceRoles.InsertRoles().then(() => {
-					console.log('Roles migrated successfully---------------------');
-
-				}).catch(err => {
-					console.log(err);
-
-				});
-				console.log('User roles migration success------');
-
-			}
-		}
-		).catch(err => {
-			console.log(err);
-		});
+	private migrateWorkspacePermissions = async () => {
+		await workspaceRoleModel.InsertRoles();
 	};
 
 	migrate = async () => {
-		console.log(`Current environment----${environmentConfig.NODE_ENV}`);
-
-		this.migrateUserPermissions();
-		this.migrateWorkspacePermissions();
+		await this.migrateUserPermissions();
+		await this.migrateWorkspacePermissions();
+		console.log(`Role migration successful----${environmentConfig.NODE_ENV}`);
 	};
 
 
 }
+(async () => {
+	const roleMigrator = new RoleMigrator();
+	await roleMigrator.migrate();
+	setTimeout(() => {
+		process.exit(0);
+	}, 3000);
+})();
 
-const roleMigrator = new RoleMigrator();
-roleMigrator.migrate();
-process.on('SIGTERM', () => {
-	roleMigrator.migrate().finally(() => process.exit(0));
-});
 
-process.on('SIGINT', () => {
-	roleMigrator.migrate().finally(() => process.exit(0));
-});
-
-process.on('exit', (code: number) => {
-	console.log('APP stopped !', code);
-});
