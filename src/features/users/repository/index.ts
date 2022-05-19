c07023dc63dd08dabf4975239599fc80e1c23315
Felix Ochieng Orinda <forinda82@gmail.com>
2022-05-19 07:11:14 +0300

    Add global events and Events emmiter on New user registration oner pending invite

--- src/features/users/repository/index.ts
+++ src/features/users/repository/index.ts
@@ -1,14 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import EventBus from '@ostrich-app/services/eventBus';
 import { IUser } from '@ostrich-app-features/users/models/interfaces';
 import { IUserRepository } from '@ostrich-app-features/users/interfaces';
 import UserModel from '@ostrich-app-features/users/models';
 import UserRoleModel from '@ostrich-app-features/userRoles/models';
+import { appEvents } from '@ostrich-app/constants/events';
 import { generateGravatarUrl } from '@ostrich-app-common/gravatar';
 import mediaModel from '@ostrich-app-features/media/models';
 import workspaceInviteModel from '@ostrich-app-features/workspaceInvite/models';
 import { workspaceMemberFactory } from '@ostrich-app-factories/workspaceMember';
 import workspaceMemberModel from '@ostrich-app-features/workspaceMember/models';
+import workspaceModel from '@ostrich-app/features/workspaces/models';
 
 class UserRepository implements IUserRepository {
 	softDeleteUser = async (id: string) => {
@@ -49,6 +52,23 @@ class UserRepository implements IUserRepository {
 			await workspaceMemberModel.create({
 				...member,
 			});
+			const workspace = await workspaceModel.findById(invite.workspaceId)
+				.populate('logo', 'url -_id')
+				.populate('owner', 'email -_id') as unknown as any;
+			const queue=new EventBus(appEvents.memberJoinWorkspace);
+			queue.sendToQueue(JSON.stringify({
+				email: userData.email,
+				workspaceId: invite.workspaceId,
+				workspaceName: workspace!.name,
+			}));
+			const queue2=new EventBus(appEvents.ownerMemberJoinWorkspace);
+			queue2.sendToQueue(JSON.stringify({
+				email:workspace?.owner!.email,
+				memberName:`${userData.firstName} ${userData.lastName}`,
+				time:new Date().getTime(),
+			}));
+
+
 		}
 
 		return newUser;
