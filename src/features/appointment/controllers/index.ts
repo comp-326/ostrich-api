96001925fd5260eaefde4d99665c6be189047efb
Felix Ochieng Orinda <forinda82@gmail.com>
2022-05-22 06:18:47 +0300

    Initiate appointment routing api

--- src/features/appointment/controllers/index.ts
+++ src/features/appointment/controllers/index.ts
@@ -3,82 +3,90 @@
 import { IAppointmentController, IAppointmentUseCases } from '../interfaces';
 import { INext, IRequest, IResponse } from '@ostrich-app-common/types';
 
-class AppointmentController implements IAppointmentController{
-	constructor(private useCase: IAppointmentUseCases){}
+class AppointmentController implements IAppointmentController {
+	constructor(private useCase: IAppointmentUseCases) { }
 
-	createAppointment=async (req: IRequest, res: IResponse, next /* eslint-disable @typescript-eslint/no-explicit-any */: INext) => {
+	createAppointment = async (req: IRequest, res: IResponse, next /* eslint-disable @typescript-eslint/no-explicit-any */: INext) => {
 		try {
 			await this.useCase.createAppointment(req.body);
 
-			return res.sendStatus(200);
+			return res.sendStatus(201);
 		} catch (error) {
 			return next(error);
 		}
 	};
 
-	cancelAppointment=async (req: IRequest, res: IResponse, next /* eslint-disable @typescript-eslint/no-explicit-any */: INext) => {
+	cancelAppointment = async (req: IRequest, res: IResponse, next /* eslint-disable @typescript-eslint/no-explicit-any */: INext) => {
 		try {
+			await this.useCase.cancelAppointment(req.params.id);
+
 			return res.sendStatus(200);
 		} catch (error) {
-			return; 
+			return;
 		}
 	};
 
-	updateAppointment=async (req: IRequest, res: IResponse, next /* eslint-disable @typescript-eslint/no-explicit-any */: INext) => {
+	updateAppointment = async (req: IRequest, res: IResponse, next /* eslint-disable @typescript-eslint/no-explicit-any */: INext) => {
 		try {
+			await this.useCase.updateAppointment(req.params.id, req.body);
+
 			return res.sendStatus(200);
 		} catch (error) {
-			return; 
+			return;
 		}
 	};
 
-	getAppointmentById=async (req: IRequest, res: IResponse, next: INext) => {
+	getAppointmentById = async (req: IRequest, res: IResponse, next: INext) => {
 		try {
-			return res.sendStatus(200);
+			const appointment = await this.useCase.getAppointmentById(req.params.id);
+
+			return res.status(200).json({ data: appointment });
 		} catch (error) {
-			return; 
+			return;
 		}
 	};
 
-	getAppointments=async (req: IRequest, res: IResponse, next:
+	getAppointments = async (req: IRequest, res: IResponse, next:
 		/* eslint-disable @typescript-eslint/no-explicit-any */
 		INext) => {
 		try {
-			return res.sendStatus(200);
+			const appointments = await this.useCase.getAppointments(req.user.userId, 20, 1);
+
+			return res.status(200).json({ data: appointments });
 		} catch (error) {
-			return; 
+			return;
 		}
 	};
 
-	getCancelledAppointments=async (req: IRequest, res: IResponse, next: INext) => {
+	getCancelledAppointments = async (req: IRequest, res: IResponse, next: INext) => {
 		try {
 			return res.sendStatus(200);
 		} catch (error) {
-			return; 
+			return;
 		}
 	};
 
-	getUpcomingAppointments=async (req: IRequest, res: IResponse /* eslint-disable @typescript-eslint/no-explicit-any */, next: INext) => {
+	getUpcomingAppointments = async (req: IRequest, res: IResponse /* eslint-disable @typescript-eslint/no-explicit-any */, next: INext) => {
 		try {
 			return res.sendStatus(200);
 		} catch (error) {
-			return; 
+			return;
 		}
 	};
 
-	getPastAppointments=async (req: IRequest, res: IResponse, next: INext) => {
+	getPastAppointments = async (req: IRequest, res: IResponse, next: INext) => {
 		try {
 			return res.sendStatus(200);
 		} catch (error) {
-			return; 
+			return;
 		}
 	};
 
-	getUserAppointments=async (req: IRequest, res: IResponse, next: INext) => {
+	getUserAppointments = async (req: IRequest, res: IResponse, next: INext) => {
 		try {
 			return res.sendStatus(200);
 		} catch (error) {
-			return; 
+			return;
 		}
 	};
 
--- src/features/appointment/use-cases/index.ts
+++ src/features/appointment/use-cases/index.ts
@@ -1,16 +1,68 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { ExpressError } from '@ostrich-app/common/errors/ExpressError';
 import { IAppointment } from '../models/interfaces';
+import { createAppointmentEntity } from '../entities';
+import validateMongodbId from '@ostrich-app/utils/mongo/ObjectId-validator';
 import { IAppointmentRepository, IAppointmentUseCases } from '../interfaces';
 
 export class AppointmentUseCase implements IAppointmentUseCases {
 	constructor(private readonly repository: IAppointmentRepository) { }
 
 	createAppointment = async (appointmentData: IAppointment) => {
-		return await this.repository.create(appointmentData);
+		const { getAttendees, getDescription, getEndTime, getExpectedAttendees, getLocation, getMeetingLength, getMeetingLink, getOwner, getStartTime, getStatus, getTitle } = createAppointmentEntity(appointmentData);
+
+		return await this.repository.create({
+			attendees: getAttendees(),
+			description: getDescription(),
+			endTime: getEndTime(),
+			expectedAttendees: getExpectedAttendees(),
+			location: getLocation(),
+			meetingLength: getMeetingLength(),
+			meetingLink: getMeetingLink(),
+			owner: getOwner(),
+			startTime: getStartTime(),
+			status: getStatus(),
+			title: getTitle()
+		});
 	};
 
 	cancelAppointment = async (appointmentId: string) => {
-		return await this.repository.cancel(appointmentId);
+		if (!validateMongodbId(appointmentId)) {
+			throw new ExpressError({
+				message: 'appointmentId is not valid',
+				status: 'warning',
+				data: {},
+				statusCode: 400
+			});
+		}
+		const existingAppointment = await this.repository.findById(appointmentId);
+		if (!existingAppointment) {
+			throw new ExpressError({
+				message: 'appointment not found',
+				status: 'warning',
+				data: {},
+				statusCode: 404
+			});
+		}
+
+		const {
+			getAttendees, getDescription, getEndTime, getExpectedAttendees, getLocation, getMeetingLength, getMeetingLink, getOwner, getStartTime, getStatus, getTitle
+		} = createAppointmentEntity({ ...existingAppointment._doc, status: 'cancelled' });
+		const updatedAppointment = await this.repository.update(appointmentId, {
+			attendees: getAttendees(),
+			description: getDescription(),
+			endTime: getEndTime(),
+			expectedAttendees: getExpectedAttendees(),
+			location: getLocation(),
+			meetingLength: getMeetingLength(),
+			meetingLink: getMeetingLink(),
+			owner: getOwner(),
+			startTime: getStartTime(),
+			status: getStatus(),
+			title: getTitle()
+		});
+
+		return updatedAppointment;
 	};
 
 	updateAppointment = async (appointmentId: string, appointmentData: IAppointment) => {
