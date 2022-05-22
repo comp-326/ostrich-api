1d11b1c8da0be30aa7522a03c51899aa84abe07a
Felix Ochieng Orinda <forinda82@gmail.com>
2022-05-22 05:35:37 +0300

    Add appointments route

---
+++ src/features/appointment/routes/updateroute.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import AppointmentRepository from '../repository';
+import {AppointmentUseCase} from '../use-cases';
+import AppointmenttController from '../controllers';
+import { Router } from 'express';
+import { loginRequired } from '@ostrich-app/middlewares/Auth';
+
+
+export default function updateRoute(app:Router){
+	return (pathName:string)=>{
+		const appointmentUseCase = new AppointmentUseCase(AppointmentRepository);
+		const controller = new AppointmenttController(appointmentUseCase);
+		const appointmentRouter = Router();
+		app.use(`${pathName}`,appointmentRouter);
+		appointmentRouter.put('/appointment/update/:id',loginRequired,controller.updateAppointment);
+	};
+}
